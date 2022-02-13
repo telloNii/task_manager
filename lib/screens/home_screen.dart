ce6c9300a91c34f81cf56e9739b2a04ac6eba2db
Todd Nelson <tello_nii@outlook.com>
2022-02-13 03:35:44 +0000

    modified the home screen to house other screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:task_manager/model/operation_screens_data.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,145 +9,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int bottomNavBarIndex = 0;
+  void newIndex(int index) {
+    setState(() {
+      bottomNavBarIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      backgroundColor: const Color(0xFF4B37BA),
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: const Color(0xFF4B37BA),
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.bar_chart_rounded),
+        backgroundColor: scaffoldBackgroundColor[bottomNavBarIndex],
+        bottomNavigationBar: Theme(
+          data: ThemeData(
+            canvasColor: const Color(0xFF4B37BA),
+          ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.shifting,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: bottomNavBarIndex,
+            onTap: newIndex,
+            selectedItemColor: Colors.lightBlueAccent,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.task_rounded), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.notifications_outlined), label: ""),
+            ],
+          ),
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              padding: EdgeInsets.zero,
-              onPressed: () {},
-              icon: const CircleAvatar(
-                radius: 20,
-                backgroundImage: AssetImage("assets/images/75843984.jpeg"),
-              ),
-            ),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-        child: ListView(
-          children: [
-            const Text(
-              "Hi Todd",
-              style: TextStyle(
-                fontSize: 26.0,
-                fontFamily: "Poppins",
-                color: Colors.white,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            const Text(
-              "6 Tasks are pending",
-              style: TextStyle(
-                color: Colors.grey,
-                fontFamily: "Poppins",
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Container(
-                height: 120.0,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF5451D6),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Mobile App Design",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontFamily: "Poppins",
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const Text(
-                        "Mike and Anita",
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontFamily: "Poppins",
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Stack(
-                              children: const [
-                                CircleAvatar(
-                                  radius: 20,
-                                  backgroundImage:
-                                      AssetImage("assets/images/75843984.jpeg"),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 24.0),
-                                  child: CircleAvatar(
-                                    radius: 20,
-                                    backgroundImage:
-                                        AssetImage("assets/images/75843984.jpeg"),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const Text(
-                              "Now",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontFamily: "Poppins",
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "Monthly Review",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: "Poppins",
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.lightBlueAccent,
-                    child: Icon(
-                      Icons.calendar_today_outlined,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+        appBar: appBars.elementAt(bottomNavBarIndex),
+        body: screenData[bottomNavBarIndex]);
   }
 }
